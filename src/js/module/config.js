107020a6a084c17df00895e664d3615878b60673
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-12-18 13:23:06 +0900

    delete snapshot refactoring

---
+++ src/js/module/config.js
@@ -0,0 +1,4 @@
+export const kernelLen = 127;
+export const amp = 128;
+export const width = 256;
+export const height = 256;
--- src/js/page/Index.js
+++ src/js/page/Index.js
@@ -1,4 +1,5 @@
 import ns from '../module/ns';
+import { kernelLen, amp, width, height } from '../module/config';
 import { inv, normalize, maxIndexOf, mod } from '../module/util';
 import AnalyticSignal from '../module/analytic-signal';
 
@@ -7,31 +8,21 @@ export default () => {
   window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
   window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
 
-  ns.width = 256;
-  ns.height = 256;
-
-  ns.tWidth = 128;
-  ns.tHeight = 128;
-
-  ns.kernelLen = 127;
-  ns.amp = 128;
-
-  function initialize() {
+  const initialize = () => {
 
     var audioElm = document.getElementById("audio");
     var $output = $(".output");
     var canvasElm = document.getElementById("canvas");
     var canvasContext = canvasElm.getContext("2d");
     var timeDomainData;
-    ns.$stage = $('.snapshot');
 
-    canvasElm.width = ns.width;
-    canvasElm.height = ns.height;
+    canvasElm.width = width;
+    canvasElm.height = height;
 
     if(navigator.getUserMedia) {
       navigator.getUserMedia(
         {audio : true},
-        function(stream) {
+        (stream) => {
           var url = URL.createObjectURL(stream);
           audioElm.src = url;
           audioElm.volume = 0;
@@ -42,7 +33,7 @@ export default () => {
           timeDomainData = new Uint8Array(analyser.frequencyBinCount);
           mediastreamsource.connect(analyser);
 
-          var animation = function(){
+          const animation = () => {
             analyser.getByteFrequencyData(frequencyData);
             analyser.getByteTimeDomainData(timeDomainData);
 
@@ -51,20 +42,20 @@ export default () => {
             var baseHz = 243; // C4
             var octave = Math.log(hz/baseHz) / Math.log(2);
             var hue = mod(octave, 1) * 360;
-            ns.currentHue = hue;
+            // ns.currentHue = hue;
 
             var i, k;
-            canvasContext.clearRect(0, 0, ns.width, ns.height);
+            canvasContext.clearRect(0, 0, width, height);
             canvasContext.strokeStyle = tinycolor({ h: hue, s: 100, v: 100 }).toRgbString();
             canvasContext.beginPath();
 
-            for (var i = ns.kernelLen, l = timeDomainData.length - ns.kernelLen; i < l; i++) {
+            for (var i = kernelLen, l = timeDomainData.length - kernelLen; i < l; i++) {
               var hilbTmp = 0;
-              for(k = -ns.kernelLen; k <= ns.kernelLen; k++) {
+              for(k = -kernelLen; k <= kernelLen; k++) {
                 hilbTmp += inv(k) * (normalize(timeDomainData[i + k]) || 0);
               }
-              var x = ns.width/2 + ns.amp * normalize(timeDomainData[i]);
-              var y = ns.height/2 - ns.amp * hilbTmp;
+              var x = width/2 + amp * normalize(timeDomainData[i]);
+              var y = height/2 - amp * hilbTmp;
               canvasContext.lineTo(x, y);
             }
             canvasContext.stroke();
@@ -75,21 +66,13 @@ export default () => {
           animation();
 
         },
-        function(err) {
+        (err) => {
           console.log("The following error occured: " + err);
         }
       );
     } else {
       console.log("getUserMedia not supported");
     }
-
-    $('.btn-rec').on('click', function() {
-      var as = new AnalyticSignal(timeDomainData);
-      as.$canvas.css('left', 300 + Math.random()*500);
-      as.$canvas.css('top', Math.random()*500);
-      as.draw(ns.currentHue);
-      as.play();
-    });
   }
 
   window.addEventListener("load", initialize, false);
